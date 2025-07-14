a3141e695696a8db25d0d31b54134e57c7b04944
judywsl <j542yu@uwaterloo.ca>
2025-07-14 10:55:08 -0400

    Implement SPI data reading capabilities

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -20,12 +20,69 @@ module spi_peripheral (
     output reg [7:0] pwm_duty_cycle
 );
 
-reg [15:0] received_message; // [15] = r/w, [14:8] = register to write to, [7:0] = data bits
-reg [3:0]  bit_count; // 4-bit (hex) count from 0 to 15 to know when message is complete
+    localparam MAX_ADDRESS = 7'h04;
 
-// value-sensitive signals have N = 2 samples
-reg ncs_synch;
-reg copi_synch;
+    reg [15:0] received_message; // [15] = r/w, [14:8] = register to write to, [7:0] = data bits
+    reg [4:0]  bit_count; // count up to 16 SCLK edges to know when transaction is complete
 
-// edge-sensitive signals have N + 1 = 3 samples; sclk_synch[0] = old, sclk_synch[1] = new
-reg [1:0] sclk_synch;
+    // value-sensitive signals have N = 2 samples
+    reg ncs_synch;
+    reg copi_synch;
+
+    // edge-sensitive signals have N + 1 = 3 samples; sclk_synch[1] = old, sclk_synch[0] = new
+    reg [1:0] sclk_synch;
+
+    wire transaction_ready = (bit_count == 5'd16) ? 1 : 0;
+    wire posedge_sclk = ~sclk_synch[1] && sclk_synch[0];
+    wire negedge_sclk = sclk_synch[1] && ~sclk_synch[0];
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            received_message <= '0;
+            bit_count <= '0;
+            ncs_synch <= '0;
+            copi_synch <= '0;
+            sclk_synch <= '0;
+            en_reg_out_7_0 <= '0;
+            en_reg_out_15_8 <= '0;
+            en_reg_pwm_7_0 <= '0;
+            en_reg_pwm_15_8 <= '0;
+            pwm_duty_cycle <= '0;
+        end
+
+        else begin
+            // synchronizer FF chain to avoid metastability
+            ncs_synch <= ncs;
+            copi_synch <= copi;
+            sclk_synch[1] <= sclk;
+            sclk_synch[0] <= sclk_synch[1];
+
+
+            // SPI Mode 0: shift in data on rising SCLK, shift out data on falling SCLK
+            
+            if (!ncs_synch && !transaction_ready) begin
+                // shift in data on rising SCLK edge
+                if (posedge_sclk) begin
+                    received_message <= {received_message[14:0], copi_synch};
+                    bit_count <= bit_count + 1;
+                end
+            end
+            else if (transaction_ready) begin
+                // shift out data on falling SCLK edge if register address is valid
+                if (ncs_synch && negedge_sclk && (received_message[14:8] <= MAX_ADDRESS)) begin
+                    case (received_message[14:8])
+                        7'h00   : en_reg_out_7_0 <= received_message[7:0];
+                        7'h01   : en_reg_out_15_8 <= received_message[7:0];
+                        7'h02   : en_reg_pwm_7_0 <= received_message[7:0];
+                        7'h03   : en_reg_pwm_15_8 <= received_message[7:0];
+                        7'h04   : pwm_duty_cycle <= received_message[7:0];
+                        default :;
+                    endcase
+                end
+
+                received_message <= '0;
+                bit_count <= '0;
+            end
+        end
+    end
+endmodule
